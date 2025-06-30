acb0471fcbab66b2a0297e3bd544f540b82431cb
Area TI <equipoti@colegiostrilce.so>
2025-06-30 10:26:19 -0500

    Última modificación antes del frontend

--- trilce-app/src/main/java/com/trilce/edu/trilce_app/repository/SesionPartRepository.java
+++ trilce-app/src/main/java/com/trilce/edu/trilce_app/repository/SesionPartRepository.java
@@ -1,9 +1,13 @@
 package com.trilce.edu.trilce_app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.trilce.edu.trilce_app.model.SesionParticipante;
 import com.trilce.edu.trilce_app.model.SesionParticipanteId;
+import com.trilce.edu.trilce_app.model.Usuario;
 
 public interface SesionPartRepository extends JpaRepository<SesionParticipante, SesionParticipanteId> {
-
+    List<SesionParticipante> findByUsuario(Usuario usuario);
 }
