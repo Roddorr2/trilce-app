5b5fcde2546cfa7a44fb768d33f1e33b09b14205
Roddorr2 <150357479+Roddorr2@users.noreply.github.com>
2025-06-30 10:33:55 -0500

    Update SesionPartRepository.java

--- trilce-app/src/main/java/com/trilce/edu/trilce_app/repository/SesionPartRepository.java
+++ trilce-app/src/main/java/com/trilce/edu/trilce_app/repository/SesionPartRepository.java
@@ -2,7 +2,7 @@ package com.trilce.edu.trilce_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.trilce.edu.trilce_app.model.SesionParticipante;
+import com.trilce.edu.trilce_app.model.SesionParticipanteId;
 
 public interface SesionPartRepository extends JpaRepository<SesionParticipante, SesionParticipanteId> {
 
